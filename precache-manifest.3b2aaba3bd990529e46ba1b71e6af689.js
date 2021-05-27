81fb04c6c1c6649aa81910837caa2beeb061f463
valerianEhret <vkadirovv@gmail.com>
2021-05-27 22:12:30 +0200

    Updates

---
+++ precache-manifest.3b2aaba3bd990529e46ba1b71e6af689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9958b21e6c2210cfb3f7f1f6a4f405f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b0882fbd0224e84c219d",
+    "url": "/portfolio/static/css/main.abc2ca53.chunk.css"
+  },
+  {
+    "revision": "cb88a3af946984c958e3",
+    "url": "/portfolio/static/js/2.6b4053a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.6b4053a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0882fbd0224e84c219d",
+    "url": "/portfolio/static/js/main.09b99fa3.chunk.js"
+  },
+  {
+    "revision": "361b25be0752ed4bc489",
+    "url": "/portfolio/static/js/runtime-main.b679d28b.js"
+  }
+]);
