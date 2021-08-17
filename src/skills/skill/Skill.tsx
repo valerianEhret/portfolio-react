c101ce3bafa45390063f174ba67ff8c936887e8b
valerianEhret <vkadirovv@gmail.com>
2021-08-17 22:56:19 +0200

    refactoring Skills 2.0

--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -10,8 +10,11 @@ export function Skill(props:SkillPropsType) {
     return (
         <div className={style.skill}>
             <div className={style.icon}></div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.skillInfo} >
+                <h3 className={style.skillTitle}>{props.title}</h3>
+                <span className={style.description}>{props.description}</span>
+            </div>
+
         </div>
     )
 }
